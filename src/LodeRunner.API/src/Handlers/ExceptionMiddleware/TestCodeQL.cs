57f84b0ad89b0d185d78b7910e984847b9d8edad
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2021-10-08 14:30:01 -0500

    Test security-events

---
+++ src/LodeRunner.API/src/Handlers/ExceptionMiddleware/TestCodeQL.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LodeRunner.API.Handlers.ExceptionMiddleware
+{
+    public class TestCodeQL
+    {
+        public string Password { get; set; } = "This is a security-event test";
+
+        public void SetPassword(string pwd)
+        {
+            Password = pwd;
+        }
+
+        public string GetPassword()
+        {
+            return Password;
+        }
+    }
+}
