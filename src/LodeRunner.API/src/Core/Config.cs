6c8b7c91a3ac278411a3d8ad909d1db14e6c2981
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2021-10-13 11:39:34 -0500

    Testing LodeRunner Only - new core subfolder

--- src/LodeRunner.API/src/Core/Config.cs
+++ src/LodeRunner.API/src/Core/Config.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using LodeRunner.API.Data;
 using LodeRunner.Core.Interfaces;
 using Microsoft.Extensions.Logging;
+
 namespace LodeRunner.API
 {
     /// <summary>
@@ -36,7 +38,7 @@ namespace LodeRunner.API
         /// Gets or sets the requestLogLevel.
         /// </summary>
         public LogLevel RequestLogLevel
-        {
+         {
             get
             {
                 return this.requestLogLevel;
