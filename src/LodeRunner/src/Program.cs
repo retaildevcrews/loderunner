58857cdca0b56c6177c91b7178bf94c91725b8c7
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2021-10-13 09:24:41 -0500

    Testing LodeRunner Only - testing app exit code

--- src/LodeRunner/src/Program.cs
+++ src/LodeRunner/src/Program.cs
@@ -121,7 +121,7 @@ namespace LodeRunner
                 if (!cancelTokenSource.IsCancellationRequested)
                 {
                     cancelTokenSource.Cancel(false);
-                    Environment.Exit(-1);
+                    Environment.Exit(2);
                 }
             };
 
@@ -130,7 +130,7 @@ namespace LodeRunner
                 e.Cancel = true;
                 cancelTokenSource.Cancel(false);
 
-                Environment.Exit(-1);
+                Environment.Exit(2);
             };
         }
 
