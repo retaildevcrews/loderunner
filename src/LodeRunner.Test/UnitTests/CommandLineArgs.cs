4a3be82176344cade1d9ae15fae3d17c93f13aad
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2021-10-22 12:16:08 -0500

    Added unit test to ValidateDependencies With Invalid Data that call GetValueOrDefault.

--- src/LodeRunner.Test/UnitTests/CommandLineArgs.cs
+++ src/LodeRunner.Test/UnitTests/CommandLineArgs.cs
@@ -30,46 +30,35 @@ namespace LodeRunner.Test.UnitTests
         [Trait("Category", "Unit")]
         public void AwaitMode_Success()
         {
-            using var secretsHelper = new SecretsHelper();
+            var validArgs = new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline1.json", "--delay-start", "-1", "--secrets-volume", "secrets" };
 
-            secretsHelper.CreateValidSecrets();
+            // Assert.Equal(0, await App.Main(validArgs)); // this will start the app and try to actually run it
+            RootCommand root = App.BuildRootCommand();
 
-            try
-            {
-                var validArgs = new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline1.json", "--delay-start", "-1", "--secrets-volume", "secrets" };
+            Assert.True(root.Parse(validArgs).Errors.Count == 0, "AwaitMode Success");
 
-                // Assert.Equal(0, await App.Main(validArgs)); // this will start the app and try to actually run it
-                RootCommand root = App.BuildRootCommand();
-
-                Assert.True(root.Parse(validArgs).Errors.Count == 0, "AwaitMode Success");
-
-                // Assert.True(root.Parse("-s", "--server").Errors.Count == 0, "Server(s) to test");
-                // Assert.True(root.Parse("-f", "--files").Errors.Count == 0, "List of files to test");
-                // Assert.True(root.Parse("--zone").Errors.Count == 0, "Zone for logging");
-                // Assert.True(root.Parse("--region").Errors.Count == 0, "Region for logging");
-                // Assert.True(root.Parse("-p", "--prometheus").Errors.Count == 0, "Send metrics to Prometheus");
-                // Assert.True(root.Parse("--tag").Errors.Count == 0, "Tag for logging");
-                // Assert.True(root.Parse("-l", "--sleep").Errors.Count == 0, "Sleep (ms) between each request");
-                // Assert.True(root.Parse("-j", "--strict-json").Errors.Count == 0, "Use strict json when parsing");
-                // Assert.True(root.Parse("-u", "--base-url").Errors.Count == 0, "Base url for files");
-                // Assert.True(root.Parse("-v", "--verbose").Errors.Count == 0, "Display verbose results");
-                // Assert.True(root.Parse("-r", "--run-loop").Errors.Count == 0, "Run test in an infinite loop");
-                // Assert.True(root.Parse("--verbose-errors").Errors.Count == 0, "Log verbose error messages");
-                // Assert.True(root.Parse("--random").Errors.Count == 0, "Run requests randomly (requires --run-loop)");
-                // Assert.True(root.Parse("--duration").Errors.Count == 0, "Test duration (seconds)  (requires --run-loop)");
-                // Assert.True(root.Parse("--summary-minutes").Errors.Count == 0, "Display summary results (minutes)  (requires --run-loop)");
-                // Assert.True(root.Parse("-t", "--timeout").Errors.Count == 0, "Request timeout (seconds)");
-                // Assert.True(root.Parse("--max-concurrent").Errors.Count == 0, "Max concurrent requests");
-                // Assert.True(root.Parse("--max-errors").Errors.Count == 0, "Max validation errors");
-                // Assert.True(root.Parse("--delay-start").Errors.Count == 0, "Delay test start (seconds)");
-                // Assert.True(root.Parse("--client-refresh").Errors.Count == 0, "How often to refresh HTTP client (seconds)");
-                // Assert.True(root.Parse("-d", "--dry-run").Errors.Count == 0, "Validates configuration");
-                // Assert.True(root.Parse("--secrets-volume").Errors.Count == 0, "Secrets Volume Path");
-            }
-            finally
-            {
-                SecretsHelper.DeleteSecrets();
-            }
+            // Assert.True(root.Parse("-s", "--server").Errors.Count == 0, "Server(s) to test");
+            // Assert.True(root.Parse("-f", "--files").Errors.Count == 0, "List of files to test");
+            // Assert.True(root.Parse("--zone").Errors.Count == 0, "Zone for logging");
+            // Assert.True(root.Parse("--region").Errors.Count == 0, "Region for logging");
+            // Assert.True(root.Parse("-p", "--prometheus").Errors.Count == 0, "Send metrics to Prometheus");
+            // Assert.True(root.Parse("--tag").Errors.Count == 0, "Tag for logging");
+            // Assert.True(root.Parse("-l", "--sleep").Errors.Count == 0, "Sleep (ms) between each request");
+            // Assert.True(root.Parse("-j", "--strict-json").Errors.Count == 0, "Use strict json when parsing");
+            // Assert.True(root.Parse("-u", "--base-url").Errors.Count == 0, "Base url for files");
+            // Assert.True(root.Parse("-v", "--verbose").Errors.Count == 0, "Display verbose results");
+            // Assert.True(root.Parse("-r", "--run-loop").Errors.Count == 0, "Run test in an infinite loop");
+            // Assert.True(root.Parse("--verbose-errors").Errors.Count == 0, "Log verbose error messages");
+            // Assert.True(root.Parse("--random").Errors.Count == 0, "Run requests randomly (requires --run-loop)");
+            // Assert.True(root.Parse("--duration").Errors.Count == 0, "Test duration (seconds)  (requires --run-loop)");
+            // Assert.True(root.Parse("--summary-minutes").Errors.Count == 0, "Display summary results (minutes)  (requires --run-loop)");
+            // Assert.True(root.Parse("-t", "--timeout").Errors.Count == 0, "Request timeout (seconds)");
+            // Assert.True(root.Parse("--max-concurrent").Errors.Count == 0, "Max concurrent requests");
+            // Assert.True(root.Parse("--max-errors").Errors.Count == 0, "Max validation errors");
+            // Assert.True(root.Parse("--delay-start").Errors.Count == 0, "Delay test start (seconds)");
+            // Assert.True(root.Parse("--client-refresh").Errors.Count == 0, "How often to refresh HTTP client (seconds)");
+            // Assert.True(root.Parse("-d", "--dry-run").Errors.Count == 0, "Validates configuration");
+            // Assert.True(root.Parse("--secrets-volume").Errors.Count == 0, "Secrets Volume Path");
         }
 
         /// <summary>
@@ -84,22 +73,11 @@ namespace LodeRunner.Test.UnitTests
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--secrets-volume", "secrets" }, SystemConstants.CmdLineValidationSecretsAndInvalidDelayStartMessage, "AwaitMode - missing delay-start")]
         public void AwaitMode_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
         {
-            using var secretsHelper = new SecretsHelper();
+            RootCommand root = App.BuildRootCommand();
 
-            secretsHelper.CreateEmptySecrets();
+            var errors = root.Parse(args).Errors;
 
-            try
-            {
-                RootCommand root = App.BuildRootCommand();
-
-                var errors = root.Parse(args).Errors;
-
-                Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
-            }
-            finally
-            {
-                SecretsHelper.DeleteSecrets();
-            }
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
         }
 
         /// <summary>
@@ -113,22 +91,11 @@ namespace LodeRunner.Test.UnitTests
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--random", "true", "--run-loop", "true" }, "Validation for --random")]
         public void TraditionalMode_ValidateDependencies_Success(string[] args, string messageifFailed)
         {
-            using var secretsHelper = new SecretsHelper();
+            RootCommand root = App.BuildRootCommand();
 
-            secretsHelper.CreateEmptySecrets();
+            var errors = root.Parse(args).Errors;
 
-            try
-            {
-                RootCommand root = App.BuildRootCommand();
-
-                var errors = root.Parse(args).Errors;
-
-                Assert.True(errors.Count == 0, messageifFailed);
-            }
-            finally
-            {
-                SecretsHelper.DeleteSecrets();
-            }
+            Assert.True(errors.Count == 0, messageifFailed);
         }
 
         /// <summary>
@@ -143,21 +110,41 @@ namespace LodeRunner.Test.UnitTests
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--random", "true" }, SystemConstants.CmdLineValidationRandomAndLoopMessage, "Validation for --random")]
         public void TraditionalMode_ValidateDependencies_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
         {
-            using var secretsHelper = new SecretsHelper();
+            RootCommand root = App.BuildRootCommand();
 
-            secretsHelper.CreateEmptySecrets();
+            var errors = root.Parse(args).Errors;
+
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
+        }
+
+        /// <summary>
+        /// Test the failure case of LodeRunned in Traditional mode for Validated Dependencies for the ones we use GetValueOrDefault.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--duration", "-2" }, "must be an integer >= 1", "Argument --duration")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--secrets-volume", "go" }, "must be at least 3 characters", "Argument --secrets-volume")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-2" }, "must be an integer >= -1", "Argument --delay-start")]
+        public void TraditionalMode_ValidateDependencies_With_Invalid_Data_And_GetValueOrDefault(string[] args, string expectedErrorMessage, string messageifFailed)
+        {
+            // Note: we do not need to test for "--run-loop" and "--random" since they are both boolean type and default to "true", so exception is never thrown.
+            RootCommand root = App.BuildRootCommand();
 
             try
             {
-                RootCommand root = App.BuildRootCommand();
-
                 var errors = root.Parse(args).Errors;
-
-                Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
+                Assert.True(false, "Previous line should have thrown a General Exception based on InLineData. InlineData is not appropriate for this Unit Test.");
             }
-            finally
+            catch (Xunit.Sdk.TrueException)
             {
-                SecretsHelper.DeleteSecrets();
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Assert.True(ex.Message.Contains(expectedErrorMessage), messageifFailed);
             }
         }
 
@@ -170,25 +157,14 @@ namespace LodeRunner.Test.UnitTests
         [Trait("Category", "Unit")]
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "5" }, "Argument --delay-start")]
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-errors", "1" }, "Argument --max-errors")]
-        // TODO add individual params to test other test cases
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json" }, "Minimum requirements met")]
         public void TraditionalMode_Success(string[] args, string messageifFailed)
         {
-            using var secretsHelper = new SecretsHelper();
+            RootCommand root = App.BuildRootCommand();
 
-            secretsHelper.CreateEmptySecrets();
+            var errors = root.Parse(args).Errors;
 
-            try
-            {
-                RootCommand root = App.BuildRootCommand();
-
-                var errors = root.Parse(args).Errors;
-
-                Assert.True(errors.Count == 0, messageifFailed);
-            }
-            finally
-            {
-                SecretsHelper.DeleteSecrets();
-            }
+            Assert.True(errors.Count == 0, messageifFailed);
         }
 
         /// <summary>
@@ -199,36 +175,18 @@ namespace LodeRunner.Test.UnitTests
         /// <param name="messageifFailed">The message to display if test failed.</param>
         [Theory]
         [Trait("Category", "Unit")]
-        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-2" }, "must be an integer >= -1", "Argument --delay-start")]
         [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-errors", "-1" }, "must be an integer >= 1", "Argument --max-errors")]
+        [InlineData(new string[] { "-s", "-f", "memory-baseline.json", }, "Required argument missing for option: -s", "Minimum requirements not met for server")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f" }, "Required argument missing for option: -f", "Minimum requirements not met for --files")]
+
         // TODO add individual params to test other test cases
         public void TraditionalMode_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
         {
-            using var secretsHelper = new SecretsHelper();
+            RootCommand root = App.BuildRootCommand();
 
-            secretsHelper.CreateEmptySecrets();
-
-            try
-            {
-                RootCommand root = App.BuildRootCommand();
-
-                IReadOnlyCollection<ParseError> errors = null;
-                try
-                {
-                    errors = root.Parse(args).Errors;
-                    Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
-                }
-
-                // This validation throws an exception when parsing.
-                catch (Exception ex)
-                {
-                    Assert.True(ex.Message.Contains(expectedErrorMessage), messageifFailed);
-                }
-            }
-            finally
-            {
-                SecretsHelper.DeleteSecrets();
-            }
+            IReadOnlyCollection<ParseError> errors = null;
+            errors = root.Parse(args).Errors;
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
         }
     }
 }
