29f2c52e5964b50ce21cc8c1cc90a9a0d0396be3
gortegaMS <70150605+gortegaMS@users.noreply.github.com>
2021-10-27 10:52:14 -0500

    Adding Unit Test to L8r to test command line arguments (#68)
    * Initial commit
    
    * Added Secrets Helper,  Unit test methods cleanup,  minor refactor
    
    * Fix Linter issues
    
    * Added Theory and InlineData attributes to Test method.
    
    * Added Traditional mode tests to validate random and duration arguments.
    
    * Added TraditionalMode Success and Faliure for specific argument
    
    * Added unit test to ValidateDependencies With Invalid Data that call GetValueOrDefault.
    
    * Completed Unit Test and added InLineData for all args
    
    * Added Test project to Docker
    
    * Disabled EnableNETAnalyzers for Test project
    
    * Moved Test project into its own solution and run Unit Tests from Docker file

---
+++ src/LodeRunner.Test/Common/SecretsHelper.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LodeRunner.Test.Common
+{
+    /// <summary>
+    /// Represents secrets helper class.
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
+    internal class SecretsHelper : IDisposable
+    {
+        private const string CosmosCollection = "CosmosCollection";
+        private const string CosmosDatabase = "CosmosDatabase";
+        private const string CosmosKey = "CosmosKey";
+        private const string CosmosTestDatabase = "CosmosTestDatabase";
+        private const string CosmosUrl = "CosmosUrl";
+
+        private readonly string[] secretFiles = new string[] { CosmosCollection, CosmosDatabase, CosmosKey, CosmosTestDatabase, CosmosUrl };
+
+        /// <summary>
+        /// Creates the secrets that will pass Validation.
+        /// </summary>
+        public void CreateValidSecrets()
+        {
+            if (!Directory.Exists($"{Directory.GetCurrentDirectory()}/secrets"))
+            {
+                Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}/secrets");
+            }
+
+            foreach (var filename in this.secretFiles)
+            {
+                using StreamWriter sw = File.AppendText($"{Directory.GetCurrentDirectory()}/secrets/{filename}");
+                switch (filename)
+                {
+                    case CosmosUrl: sw.WriteLine("https://some-random-domain.documents.azure.com"); break;
+                    case CosmosKey: sw.WriteLine(RandomString(64)); break;
+                    default: sw.WriteLine(filename); break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates the secrets that will not pass Validation.
+        /// </summary>
+        public void CreateEmptySecrets()
+        {
+            if (!Directory.Exists($"{Directory.GetCurrentDirectory()}/secrets"))
+            {
+                Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}/secrets");
+            }
+
+            foreach (var filename in this.secretFiles)
+            {
+                using StreamWriter sw = File.CreateText($"{Directory.GetCurrentDirectory()}/secrets/{filename}");
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Dispose any objects here.
+        }
+
+        /// <summary>
+        /// Deletes the secrets.
+        /// </summary>
+        internal static void DeleteSecrets()
+        {
+            if (Directory.Exists($"{Directory.GetCurrentDirectory()}/secrets"))
+            {
+                Directory.Delete($"{Directory.GetCurrentDirectory()}/secrets", true);
+            }
+        }
+
+        /// <summary>
+        /// Creates a random string.
+        /// </summary>
+        /// <param name="length">The length.</param>
+        /// <returns>Random string.</returns>
+        private static string RandomString(int length)
+        {
+            Random random = new ();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ src/LodeRunner.Test/UnitTests/CommandLineArgs.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LodeRunner.Test.Common;
+using Xunit;
+
+namespace LodeRunner.Test.UnitTests
+{
+    /// <summary>
+    /// Command Line Arguments Unit Test.
+    /// </summary>
+    public class CommandLineArgs
+    {
+        /// <summary>
+        /// Test the success case of LodeRunner in Await mode.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void AwaitMode_Success()
+        {
+            using var secretsHelper = new SecretsHelper();
+
+            secretsHelper.CreateValidSecrets();
+            try
+            {
+                var validArgs = new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-1", "--secrets-volume", "secrets" };
+
+                RootCommand root = App.BuildRootCommand();
+
+                Assert.True(root.Parse(validArgs).Errors.Count == 0, "All arguments supplied.");
+            }
+            finally
+            {
+                SecretsHelper.DeleteSecrets();
+            }
+        }
+
+        /// <summary>
+        /// Test the failure case of LodeRunner in Await mode.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-1" }, SystemConstants.CmdLineValidationDelayStartAndEmptySecretsMessage, "secrets-volume is not supplied")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--secrets-volume", "secrets" }, SystemConstants.CmdLineValidationSecretsAndInvalidDelayStartMessage, "delay-start is not supplied")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-1", "--secrets-volume", "secrets" }, SystemConstants.CmdLineValidationSecretsVolumeEndMessage, "secrets folder does not exist")]
+        public void AwaitMode_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
+        {
+            using var secretsHelper = new SecretsHelper();
+
+            SecretsHelper.DeleteSecrets();
+
+            RootCommand root = App.BuildRootCommand();
+
+            var errors = root.Parse(args).Errors;
+
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
+        }
+
+        /// <summary>
+        /// Test the success case of LodeRunner in Traditional mode.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--duration", "1", "--run-loop", "true" }, "Validation for --duration.")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--random", "true", "--run-loop", "true" }, "Validation for --random")]
+        public void TraditionalMode_ValidateDependencies_Success(string[] args, string messageifFailed)
+        {
+            RootCommand root = App.BuildRootCommand();
+
+            var errors = root.Parse(args).Errors;
+
+            Assert.True(errors.Count == 0, messageifFailed);
+        }
+
+        /// <summary>
+        /// Test the failure case of LodeRunner in Traditional mode.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--duration", "1" }, SystemConstants.CmdLineValidationDurationAndLoopMessage, "Validation for --duration.")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--random", "true" }, SystemConstants.CmdLineValidationRandomAndLoopMessage, "Validation for --random")]
+        public void TraditionalMode_ValidateDependencies_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
+        {
+            RootCommand root = App.BuildRootCommand();
+
+            var errors = root.Parse(args).Errors;
+
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
+        }
+
+        /// <summary>
+        /// Test the failure case of LodeRunner in Traditional mode when validating dependencies for the ones that use GetValueOrDefault.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--duration", "-2" }, "must be an integer >= 1", "Argument --duration")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--secrets-volume", "go" }, "must be at least 3 characters", "Argument --secrets-volume")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "-2" }, "must be an integer >= -1", "Argument --delay-start")]
+        public void TraditionalMode_ValidateDependencies_With_Invalid_Data_And_GetValueOrDefault(string[] args, string expectedErrorMessage, string messageifFailed)
+        {
+            // Note: we do not need to test for "--run-loop" and "--random" since they are both boolean type and default to "true", so exception is never thrown.
+            RootCommand root = App.BuildRootCommand();
+
+            try
+            {
+                var errors = root.Parse(args).Errors;
+                Assert.True(false, "Previous line should have thrown a General Exception based on InLineData. InlineData is not appropriate for this Unit Test.");
+            }
+            catch (Xunit.Sdk.TrueException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Assert.True(ex.Message.Contains(expectedErrorMessage), messageifFailed);
+            }
+        }
+
+        /// <summary>
+        /// Test the success cases of LodeRunner in Traditional mode.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json" }, "Minimum requirements met")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--delay-start", "5" }, "Requirements met for argument --delay-start")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-errors", "1" }, "Requirements met for argument --max-errors")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--zone", "central" }, "Requirements met for argument --zone")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--region", "US-central" }, "Requirements met for argument --region")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--prometheus", "true" }, "Requirements met for argument --prometheus")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--tag", "myTag" }, "Requirements met for argument --tag")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--sleep", "500" }, "Requirements met for argument --tag")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--strict-json", "false" }, "Requirements met for argument --strict-json")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--base-url", "https://" }, "Requirements met for argument --base-url")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--verbose", "true" }, "Requirements met for argument --verbose")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--run-loop", "true" }, "Requirements met for argument --run-loop")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--verbose-errors", "true" }, "Requirements met for argument --verbose-errors")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--summary-minutes", "1" }, "Requirements met for argument --summary-minutes")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--timeout", "10" }, "Requirements met for argument --timeout")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-concurrent", "10" }, "Requirements met for argument --max-concurrent")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--client-refresh", "10" }, "Requirements met for argument --client-refresh")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--dry-run", "true" }, "Requirements met for argument --dry-run")]
+        public void TraditionalMode_Success(string[] args, string messageifFailed)
+        {
+            // Note: "--random", "--duration", "--secrets-volume" and "--delay-start" are tested on different Unit Tests.
+            RootCommand root = App.BuildRootCommand();
+
+            var errors = root.Parse(args).Errors;
+
+            Assert.True(errors.Count == 0, messageifFailed);
+        }
+
+        /// <summary>
+        /// Test the failure cases of LodeRunner in Traditional mode.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        /// <param name="messageifFailed">The message to display if test failed.</param>
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(new string[] { "-s", "-f", "memory-baseline.json", }, "Required argument missing for option: -s", "Minimum requirements not met for argument -- server")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f" }, "Required argument missing for option: -f", "Minimum requirements not met for argument --files")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-errors", "-1" }, "must be an integer >= 1", "Argument --max-errors")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--zone", "5" }, "must be at least 3 characters", "Minimum requirements not met for argument --zone")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--region", "US" }, "must be at least 3 characters", "Minimum requirements not met for argument --region")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--prometheus", "2" }, "Unrecognized command or argument", "Minimum requirements not met for argument --prometheus")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--tag", "tg" }, "must be at least 3 characters", "Minimum requirements not met for argument --tag")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--sleep", "sleep" }, "must be an integer >= 1", "Minimum requirements not met for argument --sleep")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--strict-json", "1" }, "Unrecognized command or argument", "Minimum requirements not met for argument --strict-json")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--base-url", "ur" }, "must be at least 3 characters", "Minimum requirements not met for argument --base-url")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--verbose", "0" }, "Unrecognized command or argument", "Minimum requirements not met for argument --verbose")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--run-loop", "0" }, "Unrecognized command or argument", "Minimum requirements not met for argument --run-loop")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--verbose-errors", "0" }, "Unrecognized command or argument", "Minimum requirements not met for argument --verbose-errors")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--summary-minutes", "time" }, "must be an integer >= 1", "Minimum requirements not met for argument --summary-minutes")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--timeout", "-1" }, "must be an integer >= 1", "Minimum requirements not met for argument --timeout")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--max-concurrent", "-1" }, "must be an integer >= 1", "Minimum requirements not met for argument --max-concurrent")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--client-refresh", "-1" }, "must be an integer >= 1", "Minimum requirements not met for argument --client-refresh")]
+        [InlineData(new string[] { "-s", "https://somerandomdomain.com", "-f", "memory-baseline.json", "--dry-run", "0" }, "Unrecognized command or argument", "Minimum requirements not met for argument --dry-run")]
+        public void TraditionalMode_Failure(string[] args, string expectedErrorMessage, string messageifFailed)
+        {
+            // Note: "--random", "--duration", "--secrets-volume" and "--delay-start" are tested on different  Unit Tests.
+            RootCommand root = App.BuildRootCommand();
+
+            var errors = root.Parse(args).Errors;
+            Assert.True(errors.Count == 1 && errors.Any(m => m.Message.Contains(expectedErrorMessage)), messageifFailed);
+        }
+    }
+}
