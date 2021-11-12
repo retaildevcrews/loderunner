04f7a9f613b2a8739c733a72bf6dad20262aacda
johee <johchung@microsoft.com>
2021-11-12 21:48:28 +0000

    Design and implement LodeRunner.UI client list, client details, and pending feature

--- src/LodeRunner.UI/src/components/Clients/index.js
+++ src/LodeRunner.UI/src/components/Clients/index.js
@@ -1,18 +1,17 @@
 import { useContext, useState } from "react";
 import PropTypes from "prop-types";
+import CheckMark from "../CheckMark";
 import { ClientContext } from "../../contexts";
-import { CLIENT } from "../../models";
-import ClientsFilter, {
-  sortByProperty,
-  filterByStatus,
-} from "../ClientsFilter";
-import { SORT_TYPES } from "../ClientsFilter/constants";
+import { CLIENT, CLIENT_STATUSES } from "../../models";
 import "./styles.css";
 
-const Clients = ({ openClientDetails }) => {
+const Clients = ({
+  setFetchClientsInterval,
+  openClientDetails,
+  openedClientDetailsIndex,
+  closeClientDetails,
+}) => {
   const { clients } = useContext(ClientContext);
-  const [filterBy, setFilterBy] = useState("");
-  const [sortBy, setSortBy] = useState(SORT_TYPES.name.key);
   const [selectedClients, setSelectedClients] = useState({});
 
   function toggleClient(loadClientId) {
@@ -22,46 +21,73 @@ const Clients = ({ openClientDetails }) => {
     });
   }
 
+  const toggleClientDetails = (index) => {
+    if (index === openedClientDetailsIndex) {
+      closeClientDetails();
+    } else {
+      openClientDetails(index);
+    }
+  };
+
   return (
-    <>
-      <h1>Clients</h1>
-      <ClientsFilter {...{ setFilterBy, setSortBy }} />
-      <ul>
-        {clients
-          .filter(filterByStatus(filterBy))
-          .sort(sortByProperty(sortBy))
-          .map((c, index) => {
-            const {
-              [CLIENT.loadClientId]: loadClientId,
-              [CLIENT.status]: status,
-              [CLIENT.name]: name,
-            } = c;
-            return (
-              <li key={loadClientId}>
-                <button
-                  type="button"
-                  onClick={() => toggleClient(loadClientId)}
-                >
-                  {name}
-                </button>
-                <button
-                  type="button"
-                  title={status}
-                  aria-label={status}
-                  onClick={() => openClientDetails(index)}
-                >
-                  Open Details
-                </button>
-              </li>
-            );
-          })}
-      </ul>
-    </>
+    <div className="clients">
+      <h1>LodeRunner</h1>
+      <h3>Client Mode</h3>
+      <select
+        defaultValue="0"
+        onChange={({ target }) => setFetchClientsInterval(target.value)}
+      >
+        <option value="0">Auto Refresh</option>
+        <option value="5000">5 seconds</option>
+        <option value="15000">15 seconds</option>
+        <option value="30000">30 seconds</option>
+        <option value="60000">1 minutes</option>
+      </select>
+      <div>
+        {clients.map((c, index) => {
+          const {
+            [CLIENT.loadClientId]: loadClientId,
+            [CLIENT.status]: status,
+            [CLIENT.name]: name,
+          } = c;
+          return (
+            <div key={loadClientId} className="clients-item">
+              <button
+                className="clients-item-select"
+                type="button"
+                onClick={() => toggleClient(loadClientId)}
+                onKeyDown={() => toggleClient(loadClientId)}
+              >
+                {name || "Unknown"}
+                {selectedClients[loadClientId] && (
+                  <CheckMark fillColor="white" width="1em" />
+                )}
+              </button>
+              <button
+                className={`clients-item-status ${
+                  status === CLIENT_STATUSES.ready ? "ready" : "pending"
+                } ${openedClientDetailsIndex === index && "selected"}`}
+                type="button"
+                title={status}
+                aria-label={status}
+                onClick={() => toggleClientDetails(index)}
+                onKeyDown={() => toggleClientDetails(index)}
+              >
+                {openedClientDetailsIndex === index && ">"}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
 Clients.propTypes = {
+  setFetchClientsInterval: PropTypes.func.isRequired,
   openClientDetails: PropTypes.func.isRequired,
+  openedClientDetailsIndex: PropTypes.number.isRequired,
+  closeClientDetails: PropTypes.func.isRequired,
 };
 
-export { Clients as default, sortByProperty, filterByStatus };
+export default Clients;
---
+++ src/LodeRunner.UI/src/contexts/pendingFeature.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const PendingFeatureContext = createContext();
+
+export default PendingFeatureContext;
