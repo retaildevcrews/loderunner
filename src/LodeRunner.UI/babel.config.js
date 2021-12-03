454e6417104866478e1e907dca8985aee439d452
johee <johchung@microsoft.com>
2021-12-03 20:46:02 +0000

    Update LodeRunner.UI with prettier formatting changes

--- src/LodeRunner.UI/babel.config.js
+++ src/LodeRunner.UI/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-    ],
-    targets: { node: 'current' },
-  }
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  targets: { node: "current" },
+};
