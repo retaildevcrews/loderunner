c04b2c44384e1b4d80cfe5611eb8b9bb67d8637d
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2021-12-09 21:28:19 +0000

    start on tests for utilities. added a few. need to fill in the rest

--- src/LodeRunner.UI/src/services/utilities.test.js
+++ src/LodeRunner.UI/src/services/utilities.test.js
@@ -1,8 +1,37 @@
-// import { getResponseBody } from "./utilities";
+import { getResponseBody } from "./utilities";
+import { Response } from "whatwg-fetch";
 
-// TODO: write tests
 describe("getResponseBody", () => {
-  it("should do the thing", () => {
-    expect(true).toEqual(false);
+  it("should return undefined when statuscode is a 204", async () => {
+    const res = new Response(null, { status: 204 });
+
+    const result = await getResponseBody(res);
+
+    expect(result).toEqual(undefined);
+  });
+
+  it("should raise an error when content-type is not handled", async () => {
+    const res = new Response("", { status: 200, headers: { "Content-Type": "application/xml" } });
+
+    await expect(getResponseBody(res)).rejects.toThrow("Unhandled response type");
+  });
+
+  it("should raise an error when status is not ok with json body", async () => {
+    const body = JSON.stringify({ hello: "world" })
+    const res = new Response(body, { status: 404, headers: { "Content-Type": "application/json" } });
+
+    await expect(getResponseBody(res)).rejects.toThrow(body);
+  });
+
+  it("should raise an error when status is not ok with text body", async () => {
+    expect(true).toEqual(true);
+  });
+
+  it("should return the text body", async () => {
+    expect(true).toEqual(true);
+  });
+
+  it("should return the json body", async () => {
+    expect(true).toEqual(true);
   });
 });
