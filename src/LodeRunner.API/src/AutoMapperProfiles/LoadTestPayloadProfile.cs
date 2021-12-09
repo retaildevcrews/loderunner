43e65e4a0b969f12a2dc963c48beae120a8d5b2f
Kushal Azim Ekram <mekram@microsoft.com>
2021-12-09 10:17:39 -0600

    Added automapper profile

---
+++ src/LodeRunner.API/src/AutoMapperProfiles/LoadTestPayloadProfile.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using AutoMapper;
+using LodeRunner.API.Extensions;
+using LodeRunner.API.Middleware;
+using LodeRunner.Core.Models;
+
+namespace LodeRunner.API.AutoMapperProfiles
+{
+    /// <summary>
+    /// Represents the LoadTestConfig Auto-mapping Profile.
+    /// </summary>
+    /// <seealso cref="AutoMapper.Profile" />
+    public class LoadTestPayloadProfile : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadTestPayloadProfile"/> class.
+        /// </summary>
+        public LoadTestPayloadProfile()
+        {
+            this.CreateMap<LoadTestConfigPayload, LoadTestConfig>().IgnoreNotProvidedProps();
+        }
+    }
+}
