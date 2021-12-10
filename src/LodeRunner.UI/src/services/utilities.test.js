7e2e5b417688168675e0f8f3a234597ed2fb71de
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2021-12-10 16:55:42 +0000

    fill in the rest of the tests

--- src/LodeRunner.UI/src/services/utilities.test.js
+++ src/LodeRunner.UI/src/services/utilities.test.js
@@ -10,28 +10,37 @@ describe("getResponseBody", () => {
     expect(result).toEqual(undefined);
   });
 
+  it("should return the text body", async () => {
+    const res = new Response("Hello, World!", { status: 200, headers: { "Content-Type": "text/plain" } });
+
+    const result = await getResponseBody(res);
+
+    expect(result).toEqual("Hello, World!");
+  });
+
+  it("should return the parsed json body", async () => {
+    const res = new Response('{"hello":"world"}', { status: 200, headers: { "Content-Type": "application/json" } });
+
+    const result = await getResponseBody(res);
+
+    expect(result).toEqual({ hello: "world" });
+  });
+
   it("should raise an error when content-type is not handled", async () => {
     const res = new Response("", { status: 200, headers: { "Content-Type": "application/xml" } });
 
     await expect(getResponseBody(res)).rejects.toThrow("Unhandled response type");
   });
 
-  it("should raise an error when status is not ok with json body", async () => {
-    const body = JSON.stringify({ hello: "world" })
-    const res = new Response(body, { status: 404, headers: { "Content-Type": "application/json" } });
+  it("should raise an error when status is not ok with json body as text", async () => {
+    const res = new Response('{"hello":"world"}', { status: 404, headers: { "Content-Type": "application/json" } });
 
-    await expect(getResponseBody(res)).rejects.toThrow(body);
+    await expect(getResponseBody(res)).rejects.toThrow('{"hello":"world"}');
   });
 
   it("should raise an error when status is not ok with text body", async () => {
-    expect(true).toEqual(true);
-  });
+    const res = new Response("Hello, World!", { status: 404, headers: { "Content-Type": "text/plain" } });
 
-  it("should return the text body", async () => {
-    expect(true).toEqual(true);
-  });
-
-  it("should return the json body", async () => {
-    expect(true).toEqual(true);
+    await expect(getResponseBody(res)).rejects.toThrow("Hello, World!");
   });
 });
