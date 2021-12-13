4a7c4d0af430459ce84988c9c639df85aa75f74a
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2021-12-13 18:27:37 +0000

    fix linter errors and run format script

--- src/LodeRunner.UI/src/services/utilities.test.js
+++ src/LodeRunner.UI/src/services/utilities.test.js
@@ -1,5 +1,5 @@
-import { getResponseBody } from "./utilities";
 import { Response } from "whatwg-fetch";
+import { getResponseBody } from "./utilities";
 
 describe("getResponseBody", () => {
   it("should return undefined when statuscode is a 204", async () => {
@@ -11,7 +11,10 @@ describe("getResponseBody", () => {
   });
 
   it("should return the text body", async () => {
-    const res = new Response("Hello, World!", { status: 200, headers: { "Content-Type": "text/plain" } });
+    const res = new Response("Hello, World!", {
+      status: 200,
+      headers: { "Content-Type": "text/plain" },
+    });
 
     const result = await getResponseBody(res);
 
@@ -19,7 +22,10 @@ describe("getResponseBody", () => {
   });
 
   it("should return the parsed json body", async () => {
-    const res = new Response('{"hello":"world"}', { status: 200, headers: { "Content-Type": "application/json" } });
+    const res = new Response('{"hello":"world"}', {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
 
     const result = await getResponseBody(res);
 
@@ -27,19 +33,30 @@ describe("getResponseBody", () => {
   });
 
   it("should raise an error when content-type is not handled", async () => {
-    const res = new Response("", { status: 200, headers: { "Content-Type": "application/xml" } });
+    const res = new Response("", {
+      status: 200,
+      headers: { "Content-Type": "application/xml" },
+    });
 
-    await expect(getResponseBody(res)).rejects.toThrow("Unhandled response type");
+    await expect(getResponseBody(res)).rejects.toThrow(
+      "Unhandled response type"
+    );
   });
 
   it("should raise an error when status is not ok with json body as text", async () => {
-    const res = new Response('{"hello":"world"}', { status: 404, headers: { "Content-Type": "application/json" } });
+    const res = new Response('{"hello":"world"}', {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
 
     await expect(getResponseBody(res)).rejects.toThrow('{"hello":"world"}');
   });
 
   it("should raise an error when status is not ok with text body", async () => {
-    const res = new Response("Hello, World!", { status: 404, headers: { "Content-Type": "text/plain" } });
+    const res = new Response("Hello, World!", {
+      status: 404,
+      headers: { "Content-Type": "text/plain" },
+    });
 
     await expect(getResponseBody(res)).rejects.toThrow("Hello, World!");
   });
