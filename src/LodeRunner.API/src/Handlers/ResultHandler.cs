e6de513f2a8d8f0c4cfc2aebb5e546d3194aec5b
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2022-01-10 16:20:07 -0600

    Removed Cache log information

--- src/LodeRunner.API/src/Handlers/ResultHandler.cs
+++ src/LodeRunner.API/src/Handlers/ResultHandler.cs
@@ -112,13 +112,8 @@ namespace LodeRunner.API.Middleware
         /// <returns>IActionResult.</returns>
         public static async Task<ActionResult> HandleResult<TEntity>(IEnumerable<TEntity> results, NgsaLog logger)
         {
-            // log the request
-            await logger.LogInformation(nameof(HandleResult), DataRequest);
-
             if (!results.Any())
             {
-                await logger.LogInformation(nameof(HandleResult), DataNotFound);
-
                 return new NoContentResult();
             }
 
@@ -136,13 +131,8 @@ namespace LodeRunner.API.Middleware
         /// </returns>
         public static async Task<ActionResult> HandleResult<TEntity>(TEntity results, NgsaLog logger)
         {
-            // log the request
-            await logger.LogInformation(nameof(HandleResult), DataRequest);
-
             if (results == null)
             {
-                await logger.LogInformation(nameof(HandleResult), DataNotFound);
-
                 return await ResultHandler.CreateErrorResult(DataNotFound, HttpStatusCode.NotFound);
             }
 
