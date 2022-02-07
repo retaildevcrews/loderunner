ed1230ab5aff4bc1bbf3f45e3f2f3e4b3dd1965a
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2022-02-07 14:16:15 -0600

    Created stub class for TestRunExecution Integration Test

---
+++ src/LodeRunner.API.Test/IntegrationTests/ExecutingTestRun/TestRunExecution.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using LodeRunner.Core.Automapper;
+using LodeRunner.Core.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LodeRunner.API.Test.IntegrationTests.ExecutingTestRun
+{
+    /// <summary>
+    /// Represents LoadTestConfigs.
+    /// </summary>
+    public class TestRunExecution : IClassFixture<ApiWebApplicationFactory<Startup>>
+    {
+        private const string TestRunsUri = "/api/TestRuns";
+
+        private readonly ApiWebApplicationFactory<Startup> factory;
+
+        private readonly JsonSerializerOptions jsonOptions;
+
+        private readonly ITestOutputHelper output;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestRunExecution"/> class.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        /// <param name="output">The output.</param>
+        public TestRunExecution(ApiWebApplicationFactory<Startup> factory, ITestOutputHelper output)
+        {
+            this.factory = factory;
+
+            this.output = output;
+
+            this.jsonOptions = new ()
+            {
+                IgnoreNullValues = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+            };
+            this.jsonOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+
+        /// <summary>
+        /// Determines whether this instance [can create and execute test run].
+        /// </summary>
+        /// <returns><see cref="Task"/> representing the asynchronous integration test.</returns>
+        [Trait("Category", "Integration")]
+        public async Task CanCreateAndExecuteTestRun()
+        {
+            using var httpClient = ComponentsFactory.CreateLodeRunnerAPIHttpClient(this.factory);
+
+            TestRunPayload testRunPayload = null; // TODO: Create Test Run read from file.   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            HttpResponseMessage postedResponse = await httpClient.PostEntity<TestRun, TestRunPayload>(testRunPayload, TestRunsUri, this.output);
+            Assert.Equal(HttpStatusCode.Created, postedResponse.StatusCode);
+
+            var postedTestRun = await postedResponse.Content.ReadFromJsonAsync<TestRun>(this.jsonOptions);
+            var gottenHttpResponse = await httpClient.GetItemById<TestRun>(TestRunsUri, postedTestRun.Id, this.output);
+
+            Assert.Equal(HttpStatusCode.OK, gottenHttpResponse.StatusCode);
+            var gottenTestRun = await gottenHttpResponse.Content.ReadFromJsonAsync<LoadTestConfig>(this.jsonOptions);
+
+            Assert.Equal(JsonSerializer.Serialize(postedTestRun), JsonSerializer.Serialize(gottenTestRun));
+
+            // TODO:
+            // point to local file to run against local LodeRunner.API instance(3 or less transactions)  .. LR needs to point a local file to run against the API ?
+            // Wait for test execution to complete
+            // Check expected results
+
+            // TODO:  Delete the TestRun ? <<<<<<<<<<<<  this could be a different test since we need to check for Completed DateTime
+            await httpClient.DeleteItemById<TestRun>(TestRunsUri, gottenTestRun.Id, this.output);
+        }
+    }
+}
