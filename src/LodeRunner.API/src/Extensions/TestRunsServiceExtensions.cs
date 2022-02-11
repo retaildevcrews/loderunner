571337a57835388bcc0afa05d4fa4a2620e8b7c1
Nsikan Udoyen <nsikanudoyen@microsoft.com>
2022-02-11 16:46:14 -0600

    fixed errors

--- src/LodeRunner.API/src/Extensions/TestRunsServiceExtensions.cs
+++ src/LodeRunner.API/src/Extensions/TestRunsServiceExtensions.cs
@@ -5,7 +5,6 @@ using System;
 using System.Net;
 using System.Threading.Tasks;
 using LodeRunner.Core.Models;
-using LodeRunner.Core.Responses;
 using LodeRunner.Data.Interfaces;
 using LodeRunner.Services;
 using Microsoft.Azure.Cosmos;
@@ -21,40 +20,40 @@ namespace LodeRunner.API.Extensions
         /// <param name="testRunService">The client status service.</param>
         /// <param name="testRunId">The testRunId.</param>
         /// <returns>The Task</returns>
-        public static async Task<ApiResponse<TestRun>> GetTestRun(this TestRunService testRunService, string testRunId)
+        public static async Task<TestRun> GetTestRun(this TestRunService testRunService, string testRunId)
         {
-            ApiResponse<TestRun> result = new ();
+            // ApiResponse<TestRun> result = new ();
 
             // First get the object for verification
             TestRun existingTestRun = null;
-            try
-            {
-                existingTestRun = await testRunService.Get(testRunId);
+            // try
+            //{
+            existingTestRun = await testRunService.Get(testRunId);
 
-                result.Model = existingTestRun;
-                result.StatusCode = HttpStatusCode.OK;
-            }
-            catch (CosmosException cex)
+                //result.Model = existingTestRun;
+                //result.StatusCode = HttpStatusCode.OK;
+            // }
+            /*catch (CosmosException cex)
             {
                 // Returns most common Exception: 404 NotFound, 429 TooManyReqs
-                result.StatusCode = HttpStatusCode.InternalServerError;
-                result.Errors = cex.Message;
+                //result.StatusCode = HttpStatusCode.InternalServerError;
+                //result.Errors = cex.Message;
             }
             catch (Exception ex)
             {
-                result.StatusCode = HttpStatusCode.InternalServerError;
-                result.Errors = $"Unknown server exception: {ex.Message}";
-            }
+                //result.StatusCode = HttpStatusCode.InternalServerError;
+                //result.Errors = $"Unknown server exception: {ex.Message}";
+            }*/
 
             // If we get null object without exception, its 404 as well
             if (existingTestRun == null)
             {
                 // We don't have the item with specified ID, throw error
-                result.StatusCode = HttpStatusCode.NotFound;
-                result.Errors = SystemConstants.UnableToGetTestRun;
+                //result.StatusCode = HttpStatusCode.NotFound;
+                // result.Errors = SystemConstants.UnableToGetTestRun;
             }
 
-            return result;
+            return existingTestRun;
         }
 
         /// <summary>
