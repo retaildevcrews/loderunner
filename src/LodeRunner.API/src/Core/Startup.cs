efa78d9642b4ae6a1c7530d63f6e8e8d22f9be55
Alfredo Hernandez <52010147+alfredoihernandez@users.noreply.github.com>
2022-03-14 12:21:22 +0000

    wrong port specified for CORS

--- src/LodeRunner.API/src/Core/Startup.cs
+++ src/LodeRunner.API/src/Core/Startup.cs
@@ -114,7 +114,7 @@ namespace LodeRunner.API
                 int portNumber = AppConfigurationHelper.GetLoadRunnerUIPort(SystemConstants.LodeRunnerUIDefaultPort);
                 app.UseCors(builder =>
                 {
-                    builder.WithOrigins(string.Format(SystemConstants.BaseUriLocalHostPort, portNumber), "https://*.githubpreview.dev")
+                    builder.WithOrigins("https://*.githubpreview.dev")
                         .SetIsOriginAllowedToAllowWildcardSubdomains()
                         .AllowAnyMethod()
                         .AllowAnyHeader();
