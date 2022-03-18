bff7ddcce95af4e9f31b300fee7a2fd9cd9c054f
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2022-03-18 15:44:10 -0500

    Switch check from isproduction to isdevelopment

--- src/LodeRunner.API/src/Core/Startup.cs
+++ src/LodeRunner.API/src/Core/Startup.cs
@@ -201,12 +201,15 @@ namespace LodeRunner.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = SwaggerTitle, Version = "v1" });
 
-                // NOTE: this xml file documentation is needed to pull example tag from all method's documentation.
-                var filePath = Path.Combine(System.AppContext.BaseDirectory, "LodeRunnerApi.xml");
-                c.IncludeXmlComments(filePath);
+                if (AppConfigurationHelper.IsDevelopmentEnvironment())
+                {
+                    // NOTE: this xml file documentation is needed to pull example tag from all method's documentation.
+                    var filePath = Path.Combine(System.AppContext.BaseDirectory, "LodeRunnerApi.xml");
+                    c.IncludeXmlComments(filePath);
 
-                filePath = Path.Combine(System.AppContext.BaseDirectory, "LodeRunner.Core.xml");
-                c.IncludeXmlComments(filePath);
+                    filePath = Path.Combine(System.AppContext.BaseDirectory, "LodeRunner.Core.xml");
+                    c.IncludeXmlComments(filePath);
+                }
 
                 c.EnableAnnotations();
 
