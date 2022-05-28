9aa0ec6d76e3084882030068b9cafa13592a9b10
Kushal Azim Ekram <mekram@microsoft.com>
2022-05-28 23:38:49 +0000

    Permanently change shell to zsh for vscode

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,6 +2,9 @@
 
 echo "on-create start" >> ~/status
 
+# Permanently change shell to zsh for vscode user
+sudo chsh --shell /bin/zsh vscode
+
 # create local registry
 docker network create k3d
 k3d registry create registry.localhost --port 5000
