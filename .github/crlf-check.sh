3f114c55653a8eb7d09443bb6844b1456ee4409c
Nsikan Udoyen <nsikanudoyen@microsoft.com>
2022-07-01 16:53:13 -0500

    Updated crlf check script

--- .github/crlf-check.sh
+++ .github/crlf-check.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-find . -type f -iname '*.cs' -exec file -- {} ';' | grep CRLF
+files_with_crlf=$(find . -type f -iname '*.cs' -exec file -- {} ';' | grep CRLF)
+if [ -z "$files_with_crlf" ]
+then
+    echo "No files found with CRLF line endings"
+    exit 0
+else
+    echo $files_with_crlf
+    exit 1
+fi
